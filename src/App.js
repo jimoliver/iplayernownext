4e1afeba96c11d5248b70bce0d508a25ac1935d2
joliver <36411970+jimoliver@users.noreply.github.com>
2023-11-30 13:34:32 +0000

    fix: 36

--- src/App.js
+++ src/App.js
@@ -220,7 +220,7 @@ export default function App(params) {
   const demo = false;
   return (
     <Paper sx={
-      demo === true ?
+      demo === false ?
         { backgroundImage: `url(${Image})`, backgroundRepeat: 'round' }
         : { backgroundColor: 'transparent' }}>
       <Box sx={{
